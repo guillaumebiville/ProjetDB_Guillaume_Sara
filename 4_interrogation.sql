9718731a12eaaae9474b5ff5266410b4c7134b4c
guillaumebiville <guillaume.bvlle@gmail.com>
2026-03-16 22:02:16 +0100

    Update 4_interrogation.sql

--- 4_interrogation.sql
+++ 4_interrogation.sql
@@ -44,3 +44,25 @@ SELECT e.id_equipe, e.categorie_niveau, c.nom_club
 FROM EQUIPE e
 JOIN CLUB c ON e.id_club = c.id_club;
 
+-- afficher les joueurs avec le nom du club auquel ils appartiennent
+SELECT j.nom_joueur, c.nom_club
+FROM JOUEUR j
+JOIN COMPOSE cp ON j.id_joueur = cp.id_joueur
+JOIN EQUIPE e ON cp.id_equipe = e.id_equipe
+JOIN CLUB c ON e.id_club = c.id_club;
+
+-- afficher les joueurs avec leur contrat
+SELECT j.nom_joueur, c.statut_contrat, c.date_fin_contrat
+FROM JOUEUR j
+JOIN CONTRAT c ON j.id_joueur = c.id_joueur;
+
+-- afficher les joueurs qui ont participe a un match
+SELECT j.nom_joueur, cp.id_match, cp.statut_joueur_match
+FROM COMPOSE cp
+JOIN JOUEUR j ON cp.id_joueur = j.id_joueur;
+
+- afficher les matchs avec le nom du stade
+SELECT p.id_match, p.phase_competition, s.nom_stade
+FROM PARTIE p
+JOIN STADE s ON p.id_stade = s.id_stade;
+
